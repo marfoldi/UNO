d3b5696ec4701f8562a80e88aa9576b10815d0b4
Torok, Daniel Janos <daniel.torok@sap.com>
2016-10-22 15:16:31 +0200

    [Deck] fix IDeck for minimum functionality, created default implementation (Deck)

---
+++ src/main/java/hu/elte/angry/nerdz/UNO/model/deck/Deck.java
@@ -0,0 +1,64 @@
+package hu.elte.angry.nerdz.UNO.model.deck;
+
+import hu.elte.angry.nerdz.UNO.model.card.ICard;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class Deck implements IDeck {
+
+	private Stack<ICard> cards;
+	private Stack<ICard> playedCards;
+	
+	Deck(List<ICard> cards) {
+		this.cards = new Stack<ICard>();
+		this.cards.addAll(cards);
+		this.playedCards = new Stack<ICard>();
+	}
+	
+	@Override
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+
+	@Override
+	public ICard drawCard() {
+		restock(1);
+		
+		ICard drawnCard = cards.pop();
+		// Note: We should not add it to the playedCards, because these cards are still in the player's hand.
+		playedCards.push(drawnCard);
+		
+		return drawnCard;
+	}
+
+	@Override
+	public List<ICard> drawCards(int amount) {
+		restock(amount);
+		
+		ArrayList<ICard> drawnCards = new ArrayList<ICard>();
+		for (int i = 0; i < amount; i++) {
+			ICard drawnCard = cards.pop();
+			drawnCards.add(drawnCard);
+			// Note: We should not add it to the playedCards, because these cards are still in the player's hand.
+			playedCards.push(drawnCard);
+		}
+		return null;
+	}
+	
+	private void restock(int amount) {
+		// Early error handling
+		// We can not return a correct value if the deck does not contains enough number of cards.
+		if (cards.size() + playedCards.size() < amount) {
+			System.exit(1);
+		}
+		
+		cards.addAll(playedCards);
+		playedCards.clear();
+		
+		Collections.shuffle(cards);
+	}
+
+}
--- src/main/java/hu/elte/angry/nerdz/UNO/model/deck/IDeck.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/model/deck/IDeck.java
@@ -1,58 +1,11 @@
 package hu.elte.angry.nerdz.UNO.model.deck;
 
+import hu.elte.angry.nerdz.UNO.model.card.ICard;
+
+import java.util.List;
+
 public interface IDeck {
-
-  
-  ArrayList<Card> pile;
-	ArrayList<Card> cards;
-
-	public Deck(ArrayList<Card> c) {
-		cards = c;
-	}
-
-	public Deck() {
-		cards = new ArrayList<Card>(108);
-	}
-
-	public void addCard(Card c) {
-		cards.add(c);
-	}
-
-	public Card drawCard() {
-		if (cards.size() == 0) {
-			Uno.makeDeck();
-		}
-		return cards.remove(0);
-	}
-
-	public Card peek() {
-		return cards.get(0);
-	}
-
-	public void split() {
-		split((int) ((Math.random() * (cards.size() - 10)) + 10));
-	}
-
-	public void split(int c) {
-		ArrayList<Card> temp = (ArrayList) cards.subList(0, c);
-		cards.removeAll(temp);
-		cards.addAll(temp);
-	}
-
-	public void played(Card c) {
-		pile.add(c);
-	}
-
-	public boolean canHave(Card c) {
-		return cards.contains(c);
-	}
-  
-  public void shuffle() {
-		ArrayList<Card> c = new ArrayList<Card>(108);
-		while (cards.size() > 0) {
-			c.add(cards.remove((int) (cards.size() * Math.random())));
-		}
-		cards = c;
-	}
-}
+	public ICard drawCard();
+	public List<ICard> drawCards(int amount);
+	public void shuffle();
 }
