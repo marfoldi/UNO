a0b9284bc30bae3c410c43c05b1746d62221b821
Torok, Daniel Janos <daniel.torok@sap.com>
2016-10-23 01:40:12 +0200

    [Card] add matchingCard method, remove some comment

--- src/main/java/hu/elte/angry/nerdz/UNO/model/card/Card.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/model/card/Card.java
@@ -1,22 +1,55 @@
 package hu.elte.angry.nerdz.UNO.model.card;
 
+/**
+ * 
+ * @author Peter Gerencser
+ * 
+ * Default implementation for the ICard.
+ * This Card implementation can be used in the game of UNO.
+ *
+ */
 public class Card implements ICard {
 	private CardValue value;
 	private CardColor color;
 
-	public Card(CardValue v, CardColor c) {
-		this.value = v;
-		this.color = c;
+	public Card(CardValue value, CardColor color) {
+		this.value = value;
+		this.color = color;
 	}
 
+	/**
+	 * Gets the value of the card
+	 * @return
+	 */
 	@Override
 	public CardValue getValue() {
 		return value;
 	}
 
+	/**
+	 * Gets the color of the card
+	 * @return
+	 */
 	@Override
 	public CardColor getColor() {
 		return color;
+	}
+
+	/**
+	 * Determines if the card can be placed on top of the parameter card
+	 * @param card
+	 * @return
+	 */
+	@Override
+	public boolean matchingCard(ICard card) {
+		if (!(card instanceof Card)) {
+			return false;
+		}
+		
+		boolean colorCheck = color.equals(card.getColor());
+		boolean valueCheck = value.equals(card.getValue());
+		
+		return colorCheck || valueCheck;
 	}
 
 }
--- src/main/java/hu/elte/angry/nerdz/UNO/model/card/ICard.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/model/card/ICard.java
@@ -1,10 +1,32 @@
 package hu.elte.angry.nerdz.UNO.model.card;
 
+/**
+ * 
+ * @author Peter Gerencser
+ * 
+ * This interface represents a Card.
+ * Cards can be used with Decks and they are the fundamental elements of the game.
+ * Every card has a color and a value (number or activity) associated with it.
+ *
+ */
 public interface ICard {
-	// CardValue value = CardValue.EIGHT;
-	// CardColor color = CardColor.BLUE;
-
+	
+	/**
+	 * Gets the value of the card
+	 * @return
+	 */
 	public CardValue getValue();
-
+	
+	/**
+	 * Gets the color of the card
+	 * @return
+	 */
 	public CardColor getColor();
+	
+	/**
+	 * Determines if the card can be placed on top of the parameter card
+	 * @param card
+	 * @return
+	 */
+	public boolean matchingCard(ICard card);
 }
