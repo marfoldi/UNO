14b548419129e83d2825f651e9efe5ceff6f03b2
Péter Bence Márföldi <p.marfoeldi@bssn-software.de>
2016-10-23 21:21:58 +0200

    Added setters for card fields, cleaned up branch...

--- src/main/java/hu/elte/angry/nerdz/UNO/model/card/Card.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/model/card/Card.java
@@ -1,22 +1,56 @@
+/**
+ * 
+ */
 package hu.elte.angry.nerdz.UNO.model.card;
 
+/**
+ * @author gpeter1009
+ *
+ */
 public class Card implements ICard {
-	private CardValue value;
 	private CardColor color;
+	private CardValue value;
 
-	public Card(CardValue v, CardColor c) {
-		this.value = v;
-		this.color = c;
+	/**
+	 * @param color
+	 * @param value
+	 */
+	public Card(CardColor color, CardValue value) {
+		super();
+		this.color = color;
+		this.value = value;
 	}
-
+	
+	/* (non-Javadoc)
+	 * @see hu.elte.angry.nerdz.UNO.model.card.ICard#setColor(hu.elte.angry.nerdz.UNO.model.card.CardColor)
+	 */
 	@Override
-	public CardValue getValue() {
-		return value;
+	public void setColor(CardColor color) {
+		this.color = color;
 	}
-
+	
+	/* (non-Javadoc)
+	 * @see hu.elte.angry.nerdz.UNO.model.card.ICard#getColor()
+	 */
 	@Override
 	public CardColor getColor() {
 		return color;
 	}
 
+
+	/* (non-Javadoc)
+	 * @see hu.elte.angry.nerdz.UNO.model.card.ICard#setValue(hu.elte.angry.nerdz.UNO.model.card.CardValue)
+	 */
+	@Override
+	public void setValue(CardValue value) {
+		this.value = value;
+	}
+
+	/* (non-Javadoc)
+	 * @see hu.elte.angry.nerdz.UNO.model.card.ICard#getValue()
+	 */
+	@Override
+	public CardValue getValue() {
+		return value;
+	}
 }
