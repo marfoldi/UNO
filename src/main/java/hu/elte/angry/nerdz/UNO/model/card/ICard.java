9502b11a5a08d2a037311f70335f948ae7c761bf
Péter Bence Márföldi <p.marfoeldi@bssn-software.de>
2016-10-23 21:44:48 +0200

    Removed setters for card fields

--- src/main/java/hu/elte/angry/nerdz/UNO/model/card/ICard.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/model/card/ICard.java
@@ -11,23 +11,11 @@ package hu.elte.angry.nerdz.UNO.model.card;
  */
 public interface ICard {
 	/**
-	 * Sets the card color
-	 * @param color
-	 */
-	public void setColor(CardColor color);
-	
-	/**
 	 * Returns the card color
 	 * @return a CardColor object
 	 */
 	public CardColor getColor();
-	
-	/**
-	 * Sets the card value
-	 * @param value
-	 */
-	public void setValue(CardValue value);
-	
+
 	/**
 	 * Returns the card value
 	 * @return A CardValue object
