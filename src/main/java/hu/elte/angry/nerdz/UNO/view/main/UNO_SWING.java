cff560db14a342657ebbe8322af6c9fe4412d085
SYS <SYS@SYS-PC>
2016-10-28 16:06:15 +0200

    Ez már jobb kód, az importban vannak még amit nem használtam fel még,vagy ami nem kell. A képernyő felület program írása folyamatban, ránézek majd a deckre is. oda a keverő algoritmus kellene még.  A dokumnetációba is kívánok majd beleírni hogy azzal is haladjunk.

--- src/main/java/hu/elte/angry/nerdz/UNO/view/main/UNO_SWING.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/view/main/UNO_SWING.java
@@ -1,138 +1,121 @@
 package hu.elte.angry.nerdz.UNO.view.main;
 
-import javax.swing.*;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.awt.Window;
+/*@author Robert */
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+
+
+
+import java.awt.*;
 import java.awt.event.*;
-import java.awt.FlowLayout;
-import java.awt.GraphicsEnvironment;
-import java.awt.Point;
+
+
+
+import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class UNO_SWING extends javax.swing.JFrame implements ActionListener {
 
+
+public class UNO_SWING  extends JFrame { 
+
+	public UNO_SWING() {
+        
+        setTitle("Uno Card 2016 1.0");
+        setSize(650, 650);
+         
+        // Creates a menubar for a JFrame
+        JMenuBar menuBar = new JMenuBar();
+         
+        // Add the menubar to the frame
+        setJMenuBar(menuBar);
+         
+        // Define and add two drop down menu to the menubar
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenu helpMenu = new JMenu("Help");
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
+         
+        // Create and add simple menu item to one of the drop down menu
+        JMenuItem newAction = new JMenuItem("New");
+     /*   JMenuItem openAction = new JMenuItem("Open");*/
+        JMenuItem exitAction = new JMenuItem("Exit");
+       
+        JMenuItem helpAction = new JMenuItem("Help"); /*submenu*/
+        JMenuItem InfoAction = new JMenuItem("Info");    
+        
+        
+      /*  JMenuItem cutAction = new JMenuItem("Cut");
+        JMenuItem copyAction = new JMenuItem("Copy");
+        JMenuItem pasteAction = new JMenuItem("Paste");*/
+         
+        // Create and add CheckButton as a menu item to one of the drop down
+        // menu
+     
+        
+        /*JCheckBoxMenuItem checkAction = new JCheckBoxMenuItem("Check Action");
+        // Create and add Radio Buttons as simple menu items to one of the drop
+        // down menu
+        JRadioButtonMenuItem radioAction1 = new JRadioButtonMenuItem(
+                "Radio Button1");
+        JRadioButtonMenuItem radioAction2 = new JRadioButtonMenuItem(
+                "Radio Button2");
+        // Create a ButtonGroup and add both radio Button to it. Only one radio
+        // button in a ButtonGroup can be selected at a time.*/
+       
+        
+        
+        ButtonGroup bg = new ButtonGroup();
+      /*  bg.add(radioAction1);
+        bg.add(radioAction2);*/
+       
+        
+        fileMenu.add(newAction);
+        helpMenu.add(helpAction);
+        helpMenu.add(InfoAction);
+        /*fileMenu.add(openAction);
+        fileMenu.add(checkAction);*/
+       
+        
+        fileMenu.addSeparator();
+        fileMenu.add(exitAction);
+        
+        
+        /*editMenu.add(cutAction);
+        editMenu.add(copyAction);
+        editMenu.add(pasteAction);
+        editMenu.addSeparator();
+       
+        
+        editMenu.add(radioAction1);
+        editMenu.add(radioAction2);*/
+        // Add a listener to the New menu item. actionPerformed() method will
+        // invoked, if user triggred this menu item
+        
+        
+        newAction.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("You have clicked on the new action");
+            }
+        });
+    }
+		   
 	
-	
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private Object frame;
-
-
-
-
-
-	public static JMenuBar createMenuBar() {
-		
-		
-		
-		
-		
-/*	JFrame ablak;*/
-    JMenuBar menuBar;
-    JMenu menu, submenu;
-    JMenuItem menuItem;
- 
-    
-    
-
-    //Create the menu bar.
-    menuBar = new JMenuBar();
-
-    //Build the File Menu.
-    menu = new JMenu("File");
-    menu.setMnemonic(KeyEvent.VK_F);
-    menu.getAccessibleContext().setAccessibleDescription("Dealing with Files");
-    menuBar.add(menu);
-
-    //a group of JMenuItems
-    menuItem = new JMenuItem("New Game...",
-            new ImageIcon("images/newproject.png"));
-    menuItem.setMnemonic(KeyEvent.VK_P);
-    menuItem.setAccelerator(KeyStroke.getKeyStroke(
-            KeyEvent.VK_1, ActionEvent.ALT_MASK));
-    menuItem.getAccessibleContext().setAccessibleDescription(
-            "New Project");
-    menu.add(menuItem);
-
-   
-  
-
- 
-    menu.addSeparator();
-    submenu = new JMenu("A submenu");
-    submenu.setMnemonic(KeyEvent.VK_S);
-
-    menuItem = new JMenuItem("Menu item in the submenu");
-    menuItem.setAccelerator(KeyStroke.getKeyStroke(
-            KeyEvent.VK_2, ActionEvent.ALT_MASK));
-    submenu.add(menuItem);
-
-    menuItem = new JMenuItem("Another menu item");
-    submenu.add(menuItem);
-    menu.add(submenu);
-
-    //Build Edit menu in the menu bar.
-    menu = new JMenu("Edit");
-    menu.setMnemonic(KeyEvent.VK_E);
-    menu.getAccessibleContext().setAccessibleDescription(
-            "Edit Menu");
-    menuBar.add(menu);
-    return menuBar;
-	}
-
-
-
-    
-
-   /*void centerWindow(Window frame)*/
-	
-	  void setCenter(JFrame ablak)
-   {
-	  
-
-	Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-		frame = null;
-		int x = (int) ((dimension.getWidth() - ((Dimension) frame).getWidth()) / 2);
-		int y = (int) ((dimension.getHeight() - ((Dimension) frame).getHeight()) / 2);
-		
-		Point ablakCenter = new Point(x, y);
-		ablak.setLocation(ablakCenter);
-
-	  }  
-    
-
-	
-
 public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	 final JFrame frame = new JFrame("Uno Card Game");
-      frame.setJMenuBar(createMenuBar());
- 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 700);
-      
-        frame.setVisible(true);
+	UNO_SWING me = new UNO_SWING();
+    me.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    me.setVisible(true);
 }
 
 
 
 
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
 }
-  
-
-	
-}
-	
 
 
