66836af6f0063706befc5841722b27cb59e3658e
SYS <SYS@SYS-PC>
2016-11-04 20:16:25 +0100

    AboutBox and MainForm create, after I have to help, and yet exit too. The program of document will be write to be continued....

--- src/main/java/hu/elte/angry/nerdz/UNO/view/main/AboutBox.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/view/main/AboutBox.java
@@ -4,36 +4,31 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.TextArea;
 import javax.swing.*;
-
+/**
+ * 
+ * @author SYS
+ *
+ */
 public class AboutBox extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	public AboutBox ()
-	{
+	public AboutBox() {
 
-	setLayout (new GridLayout());
-    JFrame about= new JFrame();
+		setLayout(new GridLayout());
+		JFrame about = new JFrame();
 
-    setTitle("AboutBox");
-    about.setSize(400,400); 
-    about.setTitle("About");
-    about.setVisible(true);
+		setTitle("AboutBox");
+		about.setSize(400, 400);
+		about.setTitle("About");
+		about.setVisible(true);
 
-
-TextArea textArea = new TextArea();
-textArea.setText("Készitők:\r\n\r\nBónis Réka\r\nBárdi Zsolt\r\nGerencsér Péter\r\nOláh Róbert\r\nMérföldi Péter\r\nTörök Dániel");
-textArea.setEditable(false);
-about.getContentPane().add(textArea, BorderLayout.CENTER);
-about.setVisible(true);
-about.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-    }
+		TextArea textArea = new TextArea();
+		textArea.setText(
+				"Készitők:\r\n\r\nBónis Réka\r\nBárdi Zsolt\r\nGerencsér Péter\r\nOláh Róbert\r\nMérföldi Péter\r\nTörök Dániel");
+		textArea.setEditable(false);
+		about.getContentPane().add(textArea, BorderLayout.CENTER);
+		about.setVisible(true);
+		about.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+	}
 }
-
-	
-	
-
-
-	
-
-
