efd049534bcbbe171988c3c99f5695d6633abb74
SYS <SYS@SYS-PC>
2016-11-04 20:16:25 +0100

    AboutBox and MainForm create, after I have to help, and yet exit too. The program of document will be write to be continued....

--- src/main/java/hu/elte/angry/nerdz/UNO/view/main/UNO_SWING.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/view/main/UNO_SWING.java
@@ -1,151 +1,124 @@
 package hu.elte.angry.nerdz.UNO.view.main;
 
-
 /*@author Robert */
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+/**
+ * 
+ * 
+ * @author Róbert
+ *
+ */
+public class UNO_SWING extends JFrame {
 
-
-
-public class UNO_SWING  extends JFrame { 
-
-	
-	
 	JMenu file, help;
 	JMenuItem newProjekt, closeProjekt, HelpProjekt;
-	
-	
-	
-	private static final long serialVersionUID = 1L;
-	
 
+	private static final long serialVersionUID = 1L;
+	 
+	 
 	public UNO_SWING() {
-        
-        setTitle("Uno Card 2016 1.0");
-        setSize(650, 650);
-         
-        // Creates a menubar for a JFrame
-        JMenuBar menuBar = new JMenuBar();
-         
-        // Add the menubar to the frame
-        setJMenuBar(menuBar);
-         
-        // Define and add two drop down menu to the menubar
-        JMenu fileMenu = new JMenu("File");
-        JMenu helpMenu = new JMenu("Help");
-        menuBar.add(fileMenu);
-        menuBar.add(helpMenu);
-         
-        // Create and add simple menu item to one of the drop down menu
-        JMenuItem newAction = new JMenuItem("New Game");
-     /*   JMenuItem openAction = new JMenuItem("Open");*/
-        JMenuItem exitAction = new JMenuItem("Exit");
-       
-        JMenuItem helpAction = new JMenuItem("Help"); /*submenu*/
-        JMenuItem InfoAction = new JMenuItem("Info");    
-        
-        
-      /*  JMenuItem cutAction = new JMenuItem("Cut");
-        JMenuItem copyAction = new JMenuItem("Copy");
-        JMenuItem pasteAction = new JMenuItem("Paste");*/
-         
-        // Create and add CheckButton as a menu item to one of the drop down
-        // menu
-     
-        
-        /*JCheckBoxMenuItem checkAction = new JCheckBoxMenuItem("Check Action");
-        // Create and add Radio Buttons as simple menu items to one of the drop
-        // down menu
-        JRadioButtonMenuItem radioAction1 = new JRadioButtonMenuItem(
-                "Radio Button1");
-        JRadioButtonMenuItem radioAction2 = new JRadioButtonMenuItem(
-                "Radio Button2");
-        // Create a ButtonGroup and add both radio Button to it. Only one radio
-        // button in a ButtonGroup can be selected at a time.*/
-       
-        
-        
-        ButtonGroup bg = new ButtonGroup();
-      /*  bg.add(radioAction1);
-        bg.add(radioAction2);*/
-       
-        
-        fileMenu.add(newAction);
-        helpMenu.add(helpAction);
-        
-            
-        helpMenu.add(InfoAction);
-       
-        /*fileMenu.add(openAction);
-        fileMenu.add(checkAction);*/
-       
-        
-        fileMenu.addSeparator();
-        fileMenu.add(exitAction);
-        
-        fileMenu.addSeparator();
-        fileMenu.add(InfoAction);
-        
-        /*editMenu.add(cutAction);
-        editMenu.add(copyAction);
-        editMenu.add(pasteAction);
-        editMenu.addSeparator();
-       
-        
-        editMenu.add(radioAction1);
-        editMenu.add(radioAction2);*/
-        // Add a listener to the New menu item. actionPerformed() method will
-        // invoked, if user triggred this menu item
-        
-        
-        
-        
-        
-        InfoAction.addActionListener(new ActionListener() {
-       
-        	/*newAction.addActionListener(new ActionListener() {*/
-            
-        	
+
+		setTitle("Uno Card 2016 1.0");
+		setSize(650, 650);
+
+		// Creates a menubar for a JFrame
+		JMenuBar menuBar = new JMenuBar();
+
+		// Add the menubar to the frame
+		setJMenuBar(menuBar);
+
+		// Define and add two drop down menu to the menubar
+		JMenu fileMenu = new JMenu("File");
+		JMenu helpMenu = new JMenu("Help");
+		menuBar.add(fileMenu);
+		menuBar.add(helpMenu);
+
+		// Create and add simple menu item to one of the drop down menu
+		JMenuItem newAction = new JMenuItem("New Game");
+		/* JMenuItem openAction = new JMenuItem("Open"); */
+		JMenuItem exitAction = new JMenuItem("Exit");
+
+		JMenuItem helpAction = new JMenuItem("Help"); /* submenu */
+		JMenuItem InfoAction = new JMenuItem("Info");
+
+		
+	
+		new ButtonGroup();
+
+		fileMenu.add(newAction);
+		helpMenu.add(helpAction);
+
+		helpMenu.add(InfoAction);
+
+		/*
+		 * fileMenu.add(openAction); fileMenu.add(checkAction);
+		 */
+
+		fileMenu.addSeparator();
+		fileMenu.add(exitAction);
+		fileMenu.addSeparator();
+		fileMenu.add(InfoAction);
+
+				
+		
+		// Add a listener to the New menu item. actionPerformed() method will
+		// invoked, if user triggred this menu item
+
+		 InfoAction.addActionListener(new ActionListener() {
+
+			/* newAction.addActionListener(new ActionListener() { */
 
 			public void actionPerformed(ActionEvent e) {
-        		
-        	/*	AboutBox nc =new AboutBox();*/
-        		
-        				
-        		AboutBox nc = new AboutBox();
-        		
-        		
-        	/*	System.out.println("You have clicked on the new action");*/
-           
-        
-                
-                
-            
-            }
 
+			new AboutBox();
+
+			  }
+		    });
+
+		}
+
+		
 			
-        
-        
-       
-        
-        });
-    }
-		   
+		        
+			
+			 public static void main(String[] args) {
+				 UNO_SWING me = new UNO_SWING();
+					/*AboutBox ab = new AboutBox();*/
+					me.setVisible(true);
+				
+						
+			 }
+	    }
+ 
+			 
+		
+					
+						
+					
+			 
+	
+	  
+	
+
+			 
+			 
+		
+	
 
 
-public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	UNO_SWING me = new UNO_SWING();
-    me.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    me.setVisible(true);
-}
+	
+		
+	
 
 
+	
+	
+	
+	
+	
 
-
-
-}
 
 
