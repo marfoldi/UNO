f76c17b75273ed92d261fc2831a132293590d73e
gp <gpeter1009@gmail.com>
2016-11-06 20:03:42 +0100

    Asztal.java: pakli, aktuális lap. JatekosPanel.java: játékos lapjai. RobotPanel.java: robot játékos.
    Main.java, AkcioFigyelo.java: teszthez.

--- src/main/java/hu/elte/angry/nerdz/UNO/view/AkcioFigyelo.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/view/AkcioFigyelo.java
@@ -19,6 +19,7 @@ import javax.swing.JButton;
  *
  */
 public class AkcioFigyelo implements ActionListener {
+<<<<<<< Upstream, based on master/master
 	private PlayerPanel playerPanel;
 	private Desk desk;
 
@@ -82,6 +83,71 @@ public class AkcioFigyelo implements ActionListener {
 		}
 		int cardValue = rnd.nextInt(10);
 		return new Card(cardColor, CardValue.fromInt(cardValue));
+=======
+	private JatekosPanel jatekosPanel;
+	private Asztal asztal;
+
+	public AkcioFigyelo(Asztal a, JatekosPanel jp) {
+		asztal = a;
+		jatekosPanel = jp;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String label = arg0.getActionCommand();
+		if (label.equals("Új lap")) {
+			jatekosPanel.lapotHozzaad(veletlenLap());
+		} else if (asztal.felsoLap.getText().equals(((JButton) arg0.getSource()).getText())
+				|| asztal.felsoLap.getBackground().equals(((JButton) arg0.getSource()).getBackground())) {
+			jatekosPanel.lapotDob(Integer.parseInt(((JButton) arg0.getSource()).getName()));
+			CardColor szin = CardColor.YELLOW;
+			if (((JButton) arg0.getSource()).getBackground() == Color.YELLOW) {
+				szin = CardColor.YELLOW;
+			} else if (((JButton) arg0.getSource()).getBackground() == Color.RED) {
+				szin = CardColor.RED;
+			} else if (((JButton) arg0.getSource()).getBackground() == Color.GREEN) {
+				szin = CardColor.GREEN;
+			} else if (((JButton) arg0.getSource()).getBackground() == Color.BLUE) {
+				szin = CardColor.BLUE;
+			}
+			CardValue szam = CardValue.EIGHT;
+			for (CardValue cv : CardValue.values()) {
+				if (cv.toString().equals(((JButton) arg0.getSource()).getText())) {
+					szam = cv;
+				}
+			}
+			asztal.felsoLap(new Card(szin, szam));
+		}
+		jatekosPanel.lapokatMegjelenit();
+	}
+
+	/**
+	 * Véletlenszámok alapján egy lapot ad vissza. Nem végez ellenőrzést,
+	 * ugyanazt a lapot többször is visszaadhatja. Csak a teszteléshez
+	 * használom.
+	 *
+	 * @return
+	 */
+	private ICard veletlenLap() {
+		Random gen = new Random();
+		CardColor szin = null;
+		switch (gen.nextInt(4)) {
+		case 0:
+			szin = CardColor.YELLOW;
+			break;
+		case 1:
+			szin = CardColor.RED;
+			break;
+		case 2:
+			szin = CardColor.GREEN;
+			break;
+		case 3:
+			szin = CardColor.BLUE;
+			break;
+		}
+		int szam = gen.nextInt(10);
+		return new Card(szin, CardValue.fromInt(szam));
+>>>>>>> fb7db68 Asztal.java: pakli, aktuális lap. JatekosPanel.java: játékos lapjai. RobotPanel.java: robot játékos.
 	}
 
 }
--- src/main/java/hu/elte/angry/nerdz/UNO/view/Main.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/view/Main.java
@@ -2,6 +2,7 @@ package hu.elte.angry.nerdz.UNO.view;
 
 import hu.elte.angry.nerdz.UNO.model.card.*;
 
+<<<<<<< Upstream, based on master/master
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +31,61 @@ public class Main extends JFrame {
 
 	public static void main(String[] args) {
 		new Main();
+=======
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class Main extends JFrame {
+	private static JatekosPanel jatekosPanel;// ezen vannak a játékos lapjai
+	private static Asztal asztal;// ezen látszik a "húzó-pakli", és az
+									// "eldobó-pakli" tetején levő lap
+	private static RobotPanel rp1, rp2;// két robot
+
+	public Main(String string) {
+		super(string);
+		setSize(800, 600);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main("UNO");
+		main.setLayout(new GridLayout(3, 1));
+
+		rp1 = new RobotPanel("Robot1", 7);
+		rp2 = new RobotPanel("Robot2", 7);
+		JPanel ketRobot = new JPanel();
+		ketRobot.setLayout(new GridLayout(1, 2));
+		ketRobot.add(rp1);
+		ketRobot.add(rp2);
+		main.add(ketRobot);
+
+		List<ICard> kezdetiLapok = new ArrayList<ICard>();
+		kezdetiLapok.add(new Card(CardColor.YELLOW, CardValue.ONE));
+		kezdetiLapok.add(new Card(CardColor.RED, CardValue.FIVE));
+		kezdetiLapok.add(new Card(CardColor.GREEN, CardValue.TWO));
+
+		jatekosPanel = new JatekosPanel();
+		main.add(jatekosPanel);
+
+		asztal = new Asztal(jatekosPanel, new Card(CardColor.BLUE, CardValue.SEVEN));
+		main.add(asztal);
+
+		jatekosPanel.asztaltBeallit(asztal);
+		jatekosPanel.lapokatHozzaad(kezdetiLapok);
+		main.setVisible(true);
+
+		rp1.lapokSzama(10);
+
+		jatekosPanel.lapotHozzaad(new Card(CardColor.RED, CardValue.SEVEN));
+
+		jatekosPanel.lapotDob(1);
+>>>>>>> fb7db68 Asztal.java: pakli, aktuális lap. JatekosPanel.java: játékos lapjai. RobotPanel.java: robot játékos.
 	}
 
 }
