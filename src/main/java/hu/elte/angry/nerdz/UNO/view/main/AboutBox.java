e65ecf533222b1b663af252516c5d604fea04612
SYS <SYS@SYS-PC>
2016-11-06 23:47:49 +0100

    A kódok rendezése, aboutbox működés korrigalasa.

--- src/main/java/hu/elte/angry/nerdz/UNO/view/main/AboutBox.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/view/main/AboutBox.java
@@ -4,9 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.TextArea;
 import javax.swing.*;
+import java.awt.Font;
+
 /**
  * 
- * @author SYS
+ * @author Robert
  *
  */
 public class AboutBox extends JFrame {
@@ -17,18 +19,40 @@ public class AboutBox extends JFrame {
 
 		setLayout(new GridLayout());
 		JFrame about = new JFrame();
+		// JPanel
+		JPanel pnlButton = new JPanel();
+		// Buttons
 
 		setTitle("AboutBox");
-		about.setSize(400, 400);
+		about.setSize(500, 600);
 		about.setTitle("About");
-		about.setVisible(true);
+		JButton Okbutton = new JButton("OK");
 
-		TextArea textArea = new TextArea();
+		// Adding to JFrame
+		pnlButton.add(Okbutton);
+		add(pnlButton);
+
+		JPanel p2 = new JPanel();
+		JButton ok = new JButton("Ok");
+		p2.add(ok);
+		getContentPane().add(p2, "South");
+
+		TextArea textArea = new TextArea(10, 30);
+		textArea.setEditable(false);
+
+		/* textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12)); */
+
+		textArea.setFont(new Font(Font.DIALOG_INPUT, Font.PLAIN, 12));
+
+		textArea.setBounds(9, 20, 160, 203);
 		textArea.setText(
 				"Készitők:\r\n\r\nBónis Réka\r\nBárdi Zsolt\r\nGerencsér Péter\r\nOláh Róbert\r\nMérföldi Péter\r\nTörök Dániel");
 		textArea.setEditable(false);
+
 		about.getContentPane().add(textArea, BorderLayout.CENTER);
 		about.setVisible(true);
-		about.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+
+		/* about.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE); */
+
 	}
 }
--- src/main/java/hu/elte/angry/nerdz/UNO/view/main/UNO_SWING.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/view/main/UNO_SWING.java
@@ -9,10 +9,11 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+
 /**
  * 
  * 
- * @author Róbert
+ * @author Robert
  *
  */
 public class UNO_SWING extends JFrame implements ActionListener {
@@ -21,48 +22,39 @@ public class UNO_SWING extends JFrame implements ActionListener {
 	JMenuItem newProjekt, closeProjekt, HelpProjekt;
 	private AbstractButton exitAction;
 
-	
-	
-	
 	private static final long serialVersionUID = 1L;
-	 
-	
-	
-	
+
 	public void ConfirmExitDialog() {
 
-        // -------------------------------------------------------------
-        // Call super class constructor plus set the size for the Window
-        // -------------------------------------------------------------
-       
-        this.setSize(350, 150);
+		// -------------------------------------------------------------
+		// Call super class constructor plus set the size for the Window
+		// -------------------------------------------------------------
 
-        // ------------------------------------------------------------------
-        // Get the frame's content pane. It is not neccessary to set the 
-        // layout manager to BorderLayout (as I did below) since BorderLayout
-        // is the default for the frame's content pane.
-        // ------------------------------------------------------------------
-        Container contentPane = this.getContentPane();
-        contentPane.setLayout(new BorderLayout());
+		this.setSize(350, 150);
 
-        // -----------------
-        // Simple text label
-        // -----------------
-        JLabel label1 = new JLabel("Confirm Exit Dialog Example", JLabel.LEFT);
+		// ------------------------------------------------------------------
+		// Get the frame's content pane. It is not neccessary to set the
+		// layout manager to BorderLayout (as I did below) since BorderLayout
+		// is the default for the frame's content pane.
+		// ------------------------------------------------------------------
+		Container contentPane = this.getContentPane();
+		contentPane.setLayout(new BorderLayout());
 
-        // -----------------------------------------------------
-        // Add all buttons directory to the frame's content pane
-        // -----------------------------------------------------
-        contentPane.add(label1, BorderLayout.SOUTH);
-        
-    }
-	
-	
-	
-	
+		// -----------------
+		// Simple text label
+		// -----------------
+		JLabel label1 = new JLabel("Confirm Exit Dialog Example", JLabel.LEFT);
+
+		// -----------------------------------------------------
+		// Add all buttons directory to the frame's content pane
+		// -----------------------------------------------------
+		contentPane.add(label1, BorderLayout.SOUTH);
+
+	}
+
 	public UNO_SWING() {
-		
-		/*JFrame f=new JFrame();*/
+
+		/* JFrame f=new JFrame(); */
 
 		setTitle("Uno Card 2016 1.0");
 		setSize(650, 650);
@@ -84,13 +76,10 @@ public class UNO_SWING extends JFrame implements ActionListener {
 		/* JMenuItem openAction = new JMenuItem("Open"); */
 		JMenuItem exitAction = new JMenuItem("Exit");
 		exitAction.addActionListener(this);
-		
 
 		JMenuItem helpAction = new JMenuItem("Help"); /* submenu */
 		JMenuItem InfoAction = new JMenuItem("Info");
 
-		
-	
 		new ButtonGroup();
 
 		fileMenu.add(newAction);
@@ -107,109 +96,70 @@ public class UNO_SWING extends JFrame implements ActionListener {
 		fileMenu.addSeparator();
 		fileMenu.add(InfoAction);
 
-				
-		
 		// Add a listener to the New menu item. actionPerformed() method will
 		// invoked, if user triggred this menu item
 
-		 InfoAction.addActionListener(new ActionListener() {
+		InfoAction.addActionListener(new ActionListener() {
 
 			/* newAction.addActionListener(new ActionListener() { */
 
 			public void actionPerformed(ActionEvent e) {
 
-			new AboutBox();
+				new AboutBox();
 
-			  }
-		    });
+			}
+		});
 
+	}
+
+	protected void processWindowEvent(WindowEvent e) {
+
+		if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+
+			int exit = JOptionPane.showConfirmDialog(this, "Are you sure exit ?");
+			if (exit == JOptionPane.YES_OPTION) {
+				System.exit(0);
+			}
+
+		} else {
+
+			// If you do not want listeners processing the WINDOW_CLOSING
+			// events, you could this next call in an else block for the:
+			// if (e.getID() ...)
+			// statement. That way, only the other types of Window events
+			// (iconification, activation, etc.) would be sent out.
+
+			super.processWindowEvent(e);
 		}
 
-	
-			
-	  protected void processWindowEvent(WindowEvent e) {
-
-	        if (e.getID() == WindowEvent.WINDOW_CLOSING) {
-	        
-	            int exit = JOptionPane.showConfirmDialog(this, "Are you sure exit ?");
-	            if (exit == JOptionPane.YES_OPTION) {
-	                System.exit(0);
-	            }
-	            
-	        } else {
-
-	            // If you do not want listeners processing the WINDOW_CLOSING
-	            // events, you could this next call in an else block for the:
-	            //     if (e.getID() ...)
-	            // statement. That way, only the other types of Window events
-	            // (iconification, activation, etc.) would be sent out.
-
-	            super.processWindowEvent(e);
-	        }
-	    
-
-
-		      
-	  
-	exitAction.addActionListener(new ActionListener() {
+		exitAction.addActionListener(new ActionListener() {
 
 			/* newAction.addActionListener(new ActionListener() { */
 
 			public void actionPerformed(ActionEvent e) {
-				/*processWindowEvent(null);*/
-				
+				/* processWindowEvent(null); */
+
 				setDefaultCloseOperation(EXIT_ON_CLOSE);
 				processWindowEvent(null);
 				exitAction.addActionListener(this);
 
-			  }
-		    });
-
-		}
-
-	   			 public static void main(String[] args) {
-				 UNO_SWING me = new UNO_SWING();
-					/*AboutBox ab = new AboutBox();*/
-					me.setVisible(true);
-					
-			 }
-
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				exitAction.addActionListener(this);
-				System.exit(0);
-				
 			}
-	    }
- 
-			 
-		
-					
-						
-					
-			 
-	
-	  
-	
+		});
 
-			 
-			 
-		
-	
+	}
 
+	public static void main(String[] args) {
+		UNO_SWING me = new UNO_SWING();
+		/* AboutBox ab = new AboutBox(); */
+		me.setVisible(true);
 
-	
-		
-	
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		exitAction.addActionListener(this);
+		System.exit(0);
 
-	
-	
-	
-	
-	
-
-
-
+	}
+}
