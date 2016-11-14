c14daacb70f4149a8ad6853ce017bb6f65f740eb
Reka <bonisreka@gmail.com>
2016-11-14 21:56:04 +0100

    robotplayer teszt

--- src/test/java/hu/elte/angry/nerdz/UNO/player/RobotPlayerTest.java
+++ src/test/java/hu/elte/angry/nerdz/UNO/player/RobotPlayerTest.java
@@ -1,30 +1,73 @@
 package hu.elte.angry.nerdz.UNO.player;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
 import hu.elte.angry.nerdz.UNO.model.card.Card;
+import hu.elte.angry.nerdz.UNO.model.card.CardColor;
+import hu.elte.angry.nerdz.UNO.model.card.CardValue;
 import hu.elte.angry.nerdz.UNO.model.card.ICard;
-import hu.elte.angry.nerdz.UNO.model.player.IPlayer;
-import hu.elte.angry.nerdz.UNO.model.player.Player;
+import hu.elte.angry.nerdz.UNO.model.player.RobotPlayer;
 import hu.elte.angry.nerdz.UNO.model.player.StepCompletion;
 
 public class RobotPlayerTest {
 
-	private IPlayer player;
-
+	private RobotPlayer player = new RobotPlayer();
+	private String result;
+	
 	@Test
-	public void testStepIsValid() {
-		player = new Player();
-		StepCompletion sc = mock(StepCompletion.class);
-		ICard card = mock(Card.class);
+	public void testNoValidCards() throws InterruptedException{
+		ICard card = new Card(CardColor.BLACK, CardValue.EIGHT);
+		player.setCards(Collections.emptyList());
+		StepCompletion sc = new StepCompletion() {
+			
+			@Override
+			public void drop(ICard card) {
+				result = "drop";
+				System.out.println(result);
+			}
+			
+			@Override
+			public void draw() {
+				result = "draw";
+			}
+		};
 		
 		player.step(card, sc);
-		System.out.println("teszt");
+		TimeUnit.SECONDS.sleep(5);	
 		
-		assertNotNull(((Player)player).getCompletionHandler());
-		assertEquals(sc, ((Player)player).getCompletionHandler());
+		assertEquals("draw", result);
+		
+	}
+	
+	@Test
+	public void testIfHaveValidCards() throws InterruptedException{
+		ICard card = new Card(CardColor.BLACK, CardValue.EIGHT);
+		ArrayList<ICard> cards = new ArrayList<>();
+		cards.add(card);
+		player.setCards(cards);
+		StepCompletion sc = new StepCompletion() {
+			
+			@Override
+			public void drop(ICard card) {
+				result = "drop";
+			}
+			
+			@Override
+			public void draw() {
+				result = "draw";
+			}
+		};
+		
+		player.step(card, sc);
+		TimeUnit.SECONDS.sleep(5);	
+		
+		assertEquals("drop", result);
+		
 	}
 
 }
