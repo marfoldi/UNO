6d3129185dc1576c13837b62cf8b75a4b9cf1efe
SYS <SYS@SYS-PC>
2016-11-16 06:56:43 +0100

    AboutBox repaired.
    We do not need have been removed based on our discussion before with
    the Srammaster from the AbouBox.

--- src/main/java/hu/elte/angry/nerdz/UNO/view/main/AboutBox.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/view/main/AboutBox.java
@@ -12,9 +12,6 @@ import java.awt.Font;
  *
  */
 
-
-
-
 public class AboutBox extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -23,28 +20,13 @@ public class AboutBox extends JFrame {
 
 		setLayout(new GridLayout());
 		JFrame about = new JFrame();
-		// JPanel
-		JPanel pnlButton = new JPanel();
-		// Buttons
 
 		setTitle("AboutBox");
 		about.setSize(500, 600);
 		about.setTitle("About");
-		JButton Okbutton = new JButton("OK");
-
-		// Adding to JFrame
-		pnlButton.add(Okbutton);
-		add(pnlButton);
-
-		JPanel p2 = new JPanel();
-		JButton ok = new JButton("Ok");
-		p2.add(ok);
-		getContentPane().add(p2, "South");
 
 		TextArea textArea = new TextArea(10, 30);
 		textArea.setEditable(false);
-
-		/* textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12)); */
 
 		textArea.setFont(new Font(Font.DIALOG_INPUT, Font.PLAIN, 12));
 
@@ -55,8 +37,6 @@ public class AboutBox extends JFrame {
 
 		about.getContentPane().add(textArea, BorderLayout.CENTER);
 		about.setVisible(true);
-
-		/* about.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE); */
 
 	}
 }
