35ce79de946c140537d6f6acaeb235674bf48bb9
Péter Bence Márföldi <p.marfoeldi@bssn-software.de>
2016-12-12 18:52:37 +0100

    Add GameController

--- src/main/java/hu/elte/angry/nerdz/UNO/controller/GameController.java
+++ src/main/java/hu/elte/angry/nerdz/UNO/controller/GameController.java
@@ -1,5 +1,38 @@
 package hu.elte.angry.nerdz.UNO.controller;
 
-public class GameController {
+import hu.elte.angry.nerdz.UNO.model.manager.GameManager;
+import hu.elte.angry.nerdz.UNO.view.DeskPanel;
 
+/**
+ * 
+ * @author TheDago
+ *
+ */
+public class GameController implements IGameController {
+	private final DeskPanel view;
+	private final GameManager model;
+	
+	/**
+	 * @param view
+	 * @param model
+	 */
+	public GameController(DeskPanel view, GameManager model) {
+		super();
+		this.view = view;
+		this.model = model;
+	}
+
+	/**
+	 * @return the view
+	 */
+	public DeskPanel getView() {
+		return view;
+	}
+
+	/**
+	 * @return the model
+	 */
+	public GameManager getModel() {
+		return model;
+	}
 }
